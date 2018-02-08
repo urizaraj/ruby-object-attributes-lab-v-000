7f1ad932755367b71fe30e045f3a53c56a8e0b7f
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 19:47:28 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  # def initialize(name, breed)
+  #   @name = name
+  #   @breed = breed
+  # end
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  
+  
